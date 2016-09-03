b583dcb33c3e76146fa080525e83602ccf57cdd0
Andrew Engelbrecht <ae@n0r.co>
2016-09-02 20:01:58 -0400

    added main simulation loop to framework
    this allows users to focus on their object descriptions, options and
    interaction callback.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,5 @@
 var isInBrowser, makeWorld, setupGraphics;
 
 
-makeWorld = require('./makeWorld.js');
-
-isInBrowser = require('is-in-browser').default;
-if (isInBrowser === false) {
-    setupGraphics = function () {
-        "use strict";
-        console.assert(isInBrowser !== false,
-                "phyzzie: the setupGraphics function can only be used in the browser.");
-    };
-} else {
-    setupGraphics = require('./setupGraphics.js');
-}
-
-
-module.exports.makeWorld = makeWorld;
-module.exports.setupGraphics = setupGraphics;
+module.exports = require('./simulate.js');
 
---
+++ src/simulate.js
@@ -0,0 +1,90 @@
+// Copyright 2016 Andrew Engelbrecht
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+var simulate, isInBrowser, makeWorld, setupGraphics;
+
+isInBrowser = require('is-in-browser').default;
+makeWorld = require('./makeWorld.js');
+
+if (isInBrowser === false) {
+    setupGraphics = function () {
+        "use strict";
+        console.assert(isInBrowser !== false,
+                "phyzzie: the setupGraphics function can only be used in the browser.");
+    };
+} else {
+    setupGraphics = require('./setupGraphics.js');
+}
+
+
+simulate = function (thingsDescription, colorsDescription, interactionCallback, options) {
+
+    "use strict";
+
+    var world, things, renderFunc, simTime, iterate;
+
+    // todo: add assert statements
+
+    world = makeWorld(thingsDescription, options.sim);
+    things = world.getThings();
+
+    options.graphics = options.graphics || {};
+    if (options.graphics.display === true) {
+        console.assert(isInBrowser !== false, "phyzzie: display mode can only be used within the browser.");
+        renderFunc = setupGraphics(things, colorsDescription, options.graphics);
+    }
+
+
+    // virtual time for non-rendering mode only
+    simTime = 0;
+
+    iterate = function () {
+
+        var continueSim, now;
+
+        if (options.graphics.display === true) {
+
+            now = new Date();
+            continueSim = world.step(now, interactionCallback);
+
+            renderFunc();
+
+            if (continueSim === true) {
+                window.requestAnimationFrame(iterate);
+            }
+
+        } else {
+
+            // faster than real time simulation
+            simTime += options.sim.maxStepMilliseconds;
+            continueSim = world.step(simTime, interactionCallback);
+
+            if (continueSim === true) {
+                if (typeof setImmediate === 'function') {
+                    setImmediate(iterate);
+                } else {
+                    // the 0ms is actually ignored and set to about 4ms or 10ms,
+                    // depending on the browser
+                    setTimeout(iterate, 0);
+                }
+            }
+        }
+    };
+
+    iterate();
+};
+
+module.exports = simulate;
+
